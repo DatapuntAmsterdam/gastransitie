ddb4292afd34455ae2a8d17c8fd77332bb407fbe
Evert Lammerts <evert@lucipher.net>
2018-01-03 15:45:56 +0100

    openapi.yml added

--- web/app/web/urls.py
+++ web/app/web/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from . import views
 
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="web/testkaart.html")),
+    path('openapi.yml', views.OpenAPIView.as_view(), name='openapi'),
 ]
