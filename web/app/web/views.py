7ab454dac387df971cf757b738341ad7552c0ec3
Evert Lammerts <evert@lucipher.net>
2018-01-04 12:58:04 +0100

    openapi parameterized and swagger-ui added

--- web/app/web/views.py
+++ web/app/web/views.py
@@ -6,4 +6,11 @@ class OpenAPIView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        host = self.request.META['HTTP_HOST']
+        if host.startswith('localhost'):
+            context['apihost'] = 'http://' + host
+            context['oauth2host'] = 'http://localhost:8686'
+        else:
+            context['apihost'] = 'https://' + host
+            context['oauth2host'] = 'https://' + host
         return context
