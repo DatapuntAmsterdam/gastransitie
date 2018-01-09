2f420a1024be37239dc5e5e8775927bb5459b4b9
Thijs Coenen <info@thylacine.nl>
2018-01-09 18:05:58 +0100

    jenkins

--- web/deploy/import/import.sh
+++ web/deploy/import/import.sh
@@ -10,18 +10,25 @@ dc() {
 	docker-compose -p gastransitie -f ${DIR}/docker-compose.yml $*
 }
 
-trap 'dc kill ; dc rm -vf' EXIT
+trap 'dc kill ; dc rm -f' EXIT
 
 # For database backups:
 rm -rf ${DIR}/backups
 mkdir -p ${DIR}/backups
 
+echo "For debugging list volumes"
+dc down	-v
+docker volume ls
+
+echo "Building images"
 dc build
 
+echo "Bringing up and waiting for database"
 dc up -d database
 dc run importer /deploy/docker-wait.sh
 
 echo "Downloading raw datafiles from object store"
+dc run --rm importer ls /data
 dc run --rm importer python manage.py download_data
 
 echo "Importing data into database"
@@ -32,6 +39,7 @@ echo "Running backups"
 dc exec -T database backup-db.sh gastransitie
 
 echo "Remove containers and volumes."
-dc rm -vf
+dc down -v
+dc rm -f
 
 echo "Done"
