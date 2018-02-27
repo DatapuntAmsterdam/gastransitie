7419f87d61c95f01501d34b1fd756cd3a459ddae
Stephan Preeker <stephan@preeker.net>
2018-02-27 11:16:35 +0100

    api styling / docs / review

--- web/app/api/urls.py
+++ web/app/api/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url, include
-from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 
 from .views import GasAfwc2017ViewSet
 from .views import BagBuurtViewSet
@@ -8,7 +8,33 @@ from .views import EnergieLabelViewSet
 from .views import RenovatieViewSet
 from .views import BagBuurtBboxViewSet
 
-router = DefaultRouter()
+
+class EnergieTransitieAPI(routers.APIRootView):
+    """
+    Energietransitie API.
+
+    Specifieke functionaliteit voor het Energie transitie dasboard.
+    """
+
+    def get_api_root_view(self, **kwargs):
+        view = super().get_api_root_view(**kwargs)
+        cls = view.cls
+
+        class Gastransitie(cls):
+            def get_view_name(self):
+                return 'Energietransitie API'
+
+        Gastransitie.__doc__ = self.__doc__
+        return Gastransitie.as_view()
+
+
+class ApiRouter(routers.DefaultRouter):
+    """The main router"""
+    APIRootView = EnergieTransitieAPI
+
+
+router = ApiRouter()
+
 router.register('afwc', GasAfwc2017ViewSet, base_name='afwc')
 router.register('buurt', BagBuurtViewSet, base_name='buurt')
 router.register('mip', Mip2016ViewSet, base_name='mip')
