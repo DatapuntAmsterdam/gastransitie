69c50552958d327aedc565b74b02b7b61e4f3d32
Thijs Coenen <info@thylacine.nl>
2018-02-27 10:50:44 +0100

    define extra datasets, fix the cache

--- frontend/src/services/privatedatasets.js
+++ frontend/src/services/privatedatasets.js
@@ -2,8 +2,9 @@
 // Handle extracting data (for tables)
 import util from './util'
 
-/* // general data
-let allBuurten = null */
+// general data
+let buurtCache = {}
+let buurtBoundsCache = {}
 
 // geoJson cache, keyed by "buurt" (lives outside of Vuex)
 let afwcCache = {}
@@ -26,36 +27,58 @@ async function readGeojson (url) {
 
 // Energie transitie specific data endpoints (all paginated GeoJSON)
 async function getAfwc (buurt) {
-  if (!afwcCache.hasOwnProperty(buurt)) {
-    console.log('Downloading new data')
-    const geojson = await readGeojson(getUrl('/afwc/') + `?buurt=${buurt}`)
-    afwcCache[buurt] = geojson
+  if (!afwcCache[buurt]) {
+    afwcCache[buurt] = readGeojson(getUrl('/afwc/') + `?buurt=${buurt}`)
   }
   return afwcCache[buurt]
 }
 
 async function getEnergieLabel (buurt) {
-  if (!energieLabelCache.hasOwnProperty(buurt)) {
-    const geojson = await readGeojson(getUrl('/energielabel/') + `?buurt=${buurt}`)
-    energieLabelCache[buurt] = geojson
+  if (!energieLabelCache[buurt]) {
+    energieLabelCache[buurt] = readGeojson(getUrl('/energielabel/') + `?buurt=${buurt}`)
   }
   return energieLabelCache[buurt]
 }
 
 async function getMip (buurt) {
-  if (!mipCache.hasOwnProperty(buurt)) {
-    const geojson = await readGeojson(getUrl('/mip/') + `?buurt=${buurt}`)
-    mipCache[buurt] = geojson
+  if (!mipCache[buurt]) {
+    mipCache[buurt] = readGeojson(getUrl('/mip/') + `?buurt=${buurt}`)
   }
   return mipCache[buurt]
 }
 
 async function getRenovatie (buurt) {
-  if (!renovatieCache.hasOwnProperty(buurt)) {
-    const geojson = await readGeojson(getUrl('/renovatie/') + `?buurt=${buurt}`)
-    renovatieCache[buurt] = geojson
+  if (!renovatieCache[buurt]) {
+    renovatieCache[buurt] = readGeojson(getUrl('/renovatie/') + `?buurt=${buurt}`)
   }
   return renovatieCache[buurt]
+}
+
+async function _getAllBuurtBounds () {
+  // From Django GEOSGeometry.extent docs:  (xmin, ymin, xmax, ymax). I.e. (lon, lat) coordinates.
+  // This project uses Leaflet which uses (lat, lon) coordinates.
+  // So from Django we get [W, S, E, N] and Leaflet needs [S, W, N, E]
+  let geojson = await readGeojson(getUrl('/buurtbbox/'))
+  geojson.features.forEach(
+    feature => {
+      let [W, S, E, N] = feature.geometry
+      buurtBoundsCache[feature.properties.vollcode] = [[S, W], [N, E]]
+    }
+  )
+}
+
+async function getBuurtBounds (buurt) {
+  if (!buurtBoundsCache[buurt]) {
+    await _getAllBuurtBounds()
+  }
+  return buurtBoundsCache[buurt]
+}
+
+async function getBuurt (buurt) {
+  if (!buurtCache[buurt]) {
+    buurtCache[buurt] = readGeojson(getUrl('/buurt/') + `?vollcode=${buurt}`)
+  }
+  return buurtCache[buurt]
 }
 
 async function getGeojsonByName (name, buurt) {
@@ -63,16 +86,12 @@ async function getGeojsonByName (name, buurt) {
     ['afwc', getAfwc],
     ['energielabel', getEnergieLabel],
     ['mip', getMip],
-    ['renovatie', getRenovatie]
+    ['renovatie', getRenovatie],
+    ['buurtbounds', getBuurtBounds],
+    ['buurt', getBuurt]
   ])
 
   return getters.get(name)(buurt)
-}
-
-async function getBuurtBounds (buurt) {
-  let geojson = await readGeojson(getUrl('/buurtbbox/') + `?vollcode=${buurt}`)
-  let bbox = geojson.features[0].geometry
-  return [[bbox[1], bbox[0]], [bbox[3], bbox[2]]]
 }
 
 export default {
@@ -81,5 +100,6 @@ export default {
   getMip,
   getRenovatie,
   getGeojsonByName,
-  getBuurtBounds
+  getBuurtBounds,
+  getBuurt
 }
