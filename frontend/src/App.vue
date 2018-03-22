2512aee2584e4b0fdd6908545171af56e90ba261
vegter <amsjurien@gmail.com>
2018-03-22 16:12:14 +0100

    AMS style header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,20 @@
 <template>
-  <div id="app" class="container-fluid">
-    <router-view/>
+  <div>
+    <ams-header></ams-header>
+    <div id="app" class="container-fluid">
+      <router-view/>
+    </div>
   </div>
 </template>
 
 <script>
+import header from './components/Header'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    'ams-header': header
+  }
 }
 </script>
 
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,109 @@
+<template>
+
+  <div>
+    <div class="outer-wrapper">
+      <div class="grid-wrapper wrapper_12 header-wrapper ">
+        <div class="grid-container container_12 grid-header-logo ">
+          <div class="grid-zone grid_12 z-logo ">
+            <div class="grid-blok grid_9 type-logo sponsored">
+              <div class="grid-element">
+                <div class="grid-edge">
+                  <div class="grid-inside">
+                    <h1 class="sitelogo">
+                      <a class="mainlogo" href="#">
+                      <span class="logoset">
+                        <img src="/static/logos/andreas.svg" class="screen-logo" alt="Gemeente Amsterdam"/>
+                        <img src="/static/logos/andreas.png" class="alt-logo" alt="Gemeente Amsterdam"/>
+                        <img src="/static/logos/andreas-print.png" class="print-logo" alt="Gemeente Amsterdam"/>
+                      </span>
+
+                        <span class="logotexts">
+                        <span class="logotext red">Gemeente</span>
+                        <span class="logotext red">Amsterdam</span>
+                      </span>
+
+                      </a>
+                      <span class="subsitetitle">
+                      <a href="#">
+                        City Deal Aardgasloos
+                      </a>
+                    </span>
+
+                    </h1>
+
+                  </div>
+
+                </div>
+              </div>
+            </div>
+
+            <div class="grid-blok grid_3 type-nav-secundair ">
+              <div class="grid-element">
+                <div class="grid-edge">
+                  <div class="grid-inside">
+                    <nav>
+                      <ul class="links horizontal right ">
+                        <li class="">
+                          <a href="#Amsterdam" class="">
+                            <span class="linklabel">
+                              Contact
+                            </span>
+                          </a>
+                        </li>
+
+                        <li class="">
+                          <a href="#Amsterdam" class="">
+                            <span class="linklabel">
+                              Login
+                            </span>
+                          </a>
+                        </li>
+
+                      </ul>
+                    </nav>
+
+                  </div>
+
+                </div>
+              </div>
+            </div>
+
+            <div class="zone-clear clear"></div>
+          </div>
+
+          <div class="clear"></div>
+        </div>
+
+        <div class="grid-container container_12 grid-header-nav breakout-container">
+          <div class="grid-zone grid_12 z-nav ">
+            <div class="grid-blok grid_12 type-nav-primair ">
+              <div class="grid-element">
+                <div class="grid-edge">
+                  <div class="grid-inside">
+                    <a aria-expanded="" href="#" class="toggle- ">
+                      <b class="caret" aria-hidden="true"></b>
+                      <span class="toggle-text visuallyhidden"></span>
+                    </a>
+
+                  </div>
+
+                </div>
+              </div>
+            </div>
+
+            <div class="zone-clear clear"></div>
+          </div>
+
+          <div class="clear"></div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+  .mainlogo {
+    text-decoration: none;
+  }
+</style>
