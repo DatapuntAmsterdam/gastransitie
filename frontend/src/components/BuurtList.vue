50574cc4364433389729467986fddbaf44870efe
vegter <amsjurien@gmail.com>
2018-03-23 11:26:54 +0100

    Buurtlist filter

--- frontend/src/components/BuurtList.vue
+++ frontend/src/components/BuurtList.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
-    <table v-if="buurten" width="100%">
+    <div v-if="buurten">
+      <div class="rij mode_input text rij_verplicht">
+        <div class="invoer">
+          <input v-model="filterText"
+                 type="text"
+                 id="formInput"
+                 class="input"
+                 placeholder="Filtertekst">
+        </div>
+
+        <button class="action primary" @click="clearFilter()">Clear</button>
+      </div>
+    <table width="100%">
       <caption></caption>
       <thead>
       <tr>
@@ -10,7 +22,7 @@
       </tr>
       </thead>
       <tbody>
-      <tr v-for="buurt in buurten" :key="buurt.vollcode">
+      <tr v-for="buurt in filteredList" :key="buurt.vollcode">
         <td>
           <router-link :to="{path: '/factsheet/' + buurt.vollcode}">{{buurt.naam}}</router-link>
         </td>
@@ -23,17 +35,59 @@
       </tr>
       </tbody>
     </table>
+    </div>
+    <div v-else>
+      Laden...
+    </div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 
+const buurtText = v => ['naam', 'vollcode', 'landelijk'].map(key => v[key].toLowerCase()).join('.')
+
+/**
+ * autofilter timeout
+ */
+let autoFilter
+
 export default {
+  data () {
+    return {
+      filteredList: [],
+      filterText: ''
+    }
+  },
+  methods: {
+    clearFilter () {
+      this.filterText = ''
+      this.filteredList = this.buurten
+    },
+
+    filter () {
+      const filterText = this.filterText.toLowerCase()
+      this.filteredList = this.buurten.filter(v => buurtText(v).includes(filterText))
+    }
+  },
   computed: {
     ...mapGetters([
       'buurten'
     ])
+  },
+  watch: {
+    'buurten' () {
+      this.clearFilter()
+    },
+    'filterText' () {
+      if (autoFilter) {
+        clearTimeout(autoFilter)
+      }
+      autoFilter = setTimeout(() => this.filter(), 250)
+    }
+  },
+  created () {
+    this.clearFilter()
   },
   components: {
   }
