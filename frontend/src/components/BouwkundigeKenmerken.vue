6f3a2ee92c5d3befdc78af8a8f4a280de40f4355
vegter <amsjurien@gmail.com>
2018-04-09 16:13:00 +0200

    Document Bouwkundige kenmerken component

--- frontend/src/components/BouwkundigeKenmerken.vue
+++ frontend/src/components/BouwkundigeKenmerken.vue
@@ -54,14 +54,16 @@ export default {
       const verdeling = buurtData.data.bouwjaar_verdeling
       let bouwjaren = Object.keys(verdeling)
         .map(key => {
+          // key is year-year, eg 1920-1930
           const [, from, to] = key.match(/^([-]?\d+)-(\d+)$/)
+          // convert to object with from, to and count properties
           return {from, to, count: verdeling[key]}
         })
-        .sort((bj1, bj2) => bj1.from - bj2.from)
+        .sort((bj1, bj2) => bj1.from - bj2.from)  // Sort on start year ascending
 
       // Convert results to std categories
 
-      // First category starts at first non-zero time interval
+      // First category starts at first non-zero time interval that starts at least at 1920
       const firstNonZero = bouwjaren.findIndex(bj => bj.count > 0 && bj.to > 1920)
       const firstYear = firstNonZero === -1 ? bouwjaren[0].to : bouwjaren[firstNonZero].from
       // Last category starts at current decennium
@@ -69,6 +71,7 @@ export default {
       const lastYear = year - (year % 10)
 
       // Compose object
+      // Before 1920 or any more recent year (eg if < 1920 = 0 and < 1930 = 0 then only show < 1930 = 0
       this.bouwjaar = {
         [`voor ${firstYear}`]: bouwjaren.slice(0, firstNonZero).reduce((sum, bj) => sum + bj.count, 0)
       }
@@ -76,6 +79,7 @@ export default {
         .forEach(bj => {
           this.bouwjaar[`${bj.from}-${bj.to}`] = bj.count
         })
+      // After current decennium
       this.bouwjaar[`na ${lastYear}`] = bouwjaren
         .filter(bj => bj.from >= lastYear)
         .reduce((total, bj) => total + bj.count, 0)
