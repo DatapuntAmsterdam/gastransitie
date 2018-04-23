194ad012831752e31355a611dfb8550acc7542a5
Thijs Coenen <info@thylacine.nl>
2018-04-23 17:30:16 +0200

    OP-179 fix bug (caused by stijl repo reorganization)

--- frontend/src/components/Layout/LoadingComponent.vue
+++ frontend/src/components/Layout/LoadingComponent.vue
@@ -52,7 +52,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
- @import "~stijl/scss/ams-colorpalette";
+ @import "~stijl/dist/scss/ams-colorpalette";
 
  $progress-background: $ams-middengrijs;
 
